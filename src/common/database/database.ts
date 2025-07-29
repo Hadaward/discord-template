e51effccc303fdbb341edca197244bb5582413ff
Hadaward <eduardo.gimenez07@gmail.com>
2025-07-29 11:18:03 -0400

    feat(database): substitui conexão única por pool de conexões para melhor gerenciamento

--- src/common/database/database.ts
+++ src/common/database/database.ts
@@ -2,26 +2,29 @@ import mysql from "mysql2/promise";
 import "@/common/database/local";
 import chalk from "chalk";
 
-let database: mysql.Connection | null = null;
+let pool: mysql.Pool | null = null;
 
 export async function initializeDatabase() {
-	console.log(chalk.blueBright("> Connecting to the database..."));
+    console.log(chalk.blueBright("> Connecting to the database..."));
 
-	database = await mysql.createConnection({
-		host: process.env.DATABASE_HOST,
-		user: process.env.DATABASE_USER,
-		password: process.env.DATABASE_PASSWORD,
-		database: process.env.DATABASE_NAME,
-		port: Number(process.env.DATABASE_PORT),
-	});
+    pool = mysql.createPool({
+        host: process.env.DATABASE_HOST,
+        user: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE_NAME,
+        port: Number(process.env.DATABASE_PORT),
+        waitForConnections: true,
+        connectionLimit: 10,
+        queueLimit: 0,
+    });
 
-	console.log(chalk.green("> Database connected successfully."));
+    console.log(chalk.green("> Database connected successfully."));
 }
 
-export function getDatabase(): mysql.Connection {
-	if (!database) {
-		throw new Error(`[${chalk.yellow("database.getDatabase")}] Unable to access the database. Please ensure it is initialized by calling ${chalk.cyan("initializeDatabase()")} first.`);
-	}
+export function getDatabase(): mysql.Pool {
+    if (!pool) {
+        throw new Error(`[${chalk.yellow("database.getDatabase")}] Unable to access the database. Please ensure it is initialized by calling ${chalk.cyan("initializeDatabase()")} first.`);
+    }
 
-	return database;
+    return pool;
 }
